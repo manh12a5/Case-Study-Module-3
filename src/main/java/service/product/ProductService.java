ca767cb8230647af2443c2585ba7ceddd117f068
manh12a5 <manh12a5@gmail.com>
2021-02-20 18:48:46 +0700

    Add SingletonConnection.java

--- src/main/java/service/product/ProductService.java
+++ src/main/java/service/product/ProductService.java
@@ -4,7 +4,6 @@ import model.Product;
 import service.SingletonConnection;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
